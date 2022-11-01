2c16b4b4eeb352b076c6a081b5239e020329ba48
Katya Akilina <katyaartline@gmail.com>
2022-11-01 22:32:51 +0300

    fix: add https in BASE_URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:7777'
-    : 'http://api.mesto.ladykot.nomoredomains.icu';
+    : 'https://api.mesto.ladykot.nomoredomains.icu';
