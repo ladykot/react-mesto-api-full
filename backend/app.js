5f7bef11009b0472a9e6afbf2d02cae25c581ff0
Katya Akilina <katyaartline@gmail.com>
2022-11-02 02:48:06 +0300

    test: add custom cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-// const cors = require("./midlewares/cors");
+const cors = require("./midlewares/cors");
 const cors = require('cors');
 const NotFoundError = require('./errors/not-found_404');
 const { createUser, login } = require('./controllers/users');
@@ -18,12 +18,12 @@ const { requestLogger, errorLogger } = require('./midlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-// app.use(cors);
-app.use(
-  cors({
-    Origin: 'https://mesto.ladykot.nomoredomains.icu',
-  }),
-);
+app.use(cors);
+// app.use(
+//   cors({
+//     Origin: 'https://mesto.ladykot.nomoredomains.icu',
+//   }),
+// );
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
