648b54f29dd3b231bd42fb9315a1e225bbe57497
Katya Akilina <katyaartline@gmail.com>
2022-11-02 01:08:25 +0300

    test: add cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('./midlewares/cors');
+// const cors = require("./midlewares/cors");
+const cors = require('cors');
 const NotFoundError = require('./errors/not-found_404');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./midlewares/auth');
@@ -17,7 +18,12 @@ const { requestLogger, errorLogger } = require('./midlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors);
+// app.use(cors);
+app.use(
+  cors({
+    Origin: 'https://mesto.ladykot.nomoredomains.icu',
+  }),
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
